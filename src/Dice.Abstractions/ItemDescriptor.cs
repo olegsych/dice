ce2ab3efcf628e75cf0d66a01a8ec7abf4cc3a25
Oleg Sych <oleg_sych@hotmail.com>
2015-05-02 14:54:53 -0700

    - Add Name property to ItemDescriptor.

--- src/Dice.Abstractions/ItemDescriptor.cs
+++ src/Dice.Abstractions/ItemDescriptor.cs
@@ -1,6 +1,19 @@
-﻿namespace Dice.Abstractions
+﻿using System;
+
+namespace Dice.Abstractions
 {
     public abstract class ItemDescriptor
     {
+        public ItemDescriptor(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException(nameof(name) + " cannot be null or white space", nameof(name));
+            }
+
+            this.Name = name;
+        }
+
+        public string Name { get; }
     }
 }
--- test/Dice.Abstractions.Tests/ItemDescriptorTest.cs
+++ test/Dice.Abstractions.Tests/ItemDescriptorTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using Xunit;
 
 namespace Dice.Abstractions
@@ -9,6 +10,30 @@ namespace Dice.Abstractions
         public void ClassIsAbstractAndServesAsBaseClassForConcreteDescriptors()
         {
             Assert.True(typeof(ItemDescriptor).GetTypeInfo().IsAbstract);
+        }
+
+        [Fact]
+        public void ConstructorInitializesNamePropertyWithGivenValue()
+        {
+            const string expected = "ItemName";
+
+            var descriptor = new TestableItemDescriptor("ItemName");
+
+            Assert.Equal(expected, descriptor.Name);
+        }
+
+        [Theory, InlineData(null), InlineData(""), InlineData(" \t\n\r")]
+        public void ConstructorThrowsArgumentExceptionWhenNameIsNotSpecifiedCorrectly(string value)
+        {
+            var e = Assert.ThrowsAny<ArgumentException>(() => new TestableItemDescriptor(value));
+            Assert.Equal("name", e.ParamName);
+        }
+
+        private class TestableItemDescriptor : ItemDescriptor
+        {
+            public TestableItemDescriptor(string name) : base(name)
+            {
+            }
         }
     }
 }
