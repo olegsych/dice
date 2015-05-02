bc4def63ad66308df57243ac37e9ac820e45fc7b
Oleg Sych <oleg_sych@hotmail.com>
2015-05-02 15:20:11 -0700

    - Add Summary property to ItemDescriptor.

--- src/Dice.Abstractions/ItemDescriptor.cs
+++ src/Dice.Abstractions/ItemDescriptor.cs
@@ -4,16 +4,24 @@ namespace Dice.Abstractions
 {
     public abstract class ItemDescriptor
     {
-        public ItemDescriptor(string name)
+        protected ItemDescriptor(string name, string summary)
         {
             if (string.IsNullOrWhiteSpace(name))
             {
                 throw new ArgumentException(nameof(name) + " cannot be null or white space", nameof(name));
             }
 
+            if (string.IsNullOrWhiteSpace(summary))
+            {
+                throw new ArgumentException(nameof(summary) + " cannot be null or white space", nameof(summary));
+            }
+
             this.Name = name;
+            this.Summary = summary;
         }
 
         public string Name { get; }
+
+        public string Summary { get; }
     }
 }
--- test/Dice.Abstractions.Tests/ItemDescriptorTest.cs
+++ test/Dice.Abstractions.Tests/ItemDescriptorTest.cs
@@ -15,23 +15,36 @@ namespace Dice.Abstractions
         [Fact]
         public void ConstructorInitializesNamePropertyWithGivenValue()
         {
-            const string expected = "ItemName";
-
-            var descriptor = new TestableItemDescriptor("ItemName");
-
-            Assert.Equal(expected, descriptor.Name);
+            const string expectedValue = "ItemName";
+            var descriptor = new TestableItemDescriptor(expectedValue, "any");
+            Assert.Equal(expectedValue, descriptor.Name);
         }
 
         [Theory, InlineData(null), InlineData(""), InlineData(" \t\n\r")]
-        public void ConstructorThrowsArgumentExceptionWhenNameIsNotSpecifiedCorrectly(string value)
+        public void ConstructorThrowsArgumentExceptionWhenNameIsNotSpecifiedCorrectly(string name)
         {
-            var e = Assert.ThrowsAny<ArgumentException>(() => new TestableItemDescriptor(value));
+            var e = Assert.ThrowsAny<ArgumentException>(() => new TestableItemDescriptor(name, "any"));
             Assert.Equal("name", e.ParamName);
+        }
+
+        [Fact]
+        public void ConstructorInitializesSummaryPropertyWithGivenValue()
+        {
+            const string expectedValue = "Test Summary";
+            var descriptor = new TestableItemDescriptor("any", expectedValue);
+            Assert.Equal(expectedValue, descriptor.Summary);
+        }
+
+        [Theory, InlineData(null), InlineData(""), InlineData(" \t\n\r")]
+        public void ConstructorThrowsArgumentExceptionWhenSummaryIsNotSpecifiedCorrectly(string summary)
+        {
+            var e = Assert.ThrowsAny<ArgumentException>(() => new TestableItemDescriptor("any", summary));
+            Assert.Equal("summary", e.ParamName);
         }
 
         private class TestableItemDescriptor : ItemDescriptor
         {
-            public TestableItemDescriptor(string name) : base(name)
+            public TestableItemDescriptor(string name, string summary) : base(name, summary)
             {
             }
         }
