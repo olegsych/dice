8eb0c2dfdec6071b5e34cb5f587b809860fcc5cb
Oleg Sych <oleg_sych@hotmail.com>
2015-05-02 17:18:41 -0700

    - Add ProgramDescriptor class.

---
+++ src/Dice.Abstractions/ProgramDescriptor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dice.Abstractions
+{
+    public class ProgramDescriptor : ItemDescriptor
+    {
+        public ProgramDescriptor(string name, string summary, IEnumerable<CommandDescriptor> commands) 
+            : base(name, summary)
+        {
+            if (commands == null)
+            {
+                throw new ArgumentNullException(nameof(commands));
+            }
+
+            if (commands.Any(item => item == null))
+            {
+                throw new ArgumentException(nameof(commands) + " elements must not be null", nameof(commands));
+            }
+
+            this.Commands = commands;
+        }
+
+        public IEnumerable<CommandDescriptor> Commands { get; }
+    }
+}
---
+++ test/Dice.Abstractions.Tests/ProgramDescriptorTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Dice.Abstractions
+{
+    public class ProgramDescriptorTest
+    {
+        [Fact]
+        public void ConstructorInvokesBaseWithAllParameters()
+        {
+            const string expectedName = "Test Name";
+            const string expectedSummary = "Test Summary";
+
+            var descriptor = new ProgramDescriptor(expectedName, expectedSummary, Enumerable.Empty<CommandDescriptor>());
+
+            Assert.Equal(expectedName, descriptor.Name);
+            Assert.Equal(expectedSummary, descriptor.Summary);
+        }
+
+        [Fact]
+        public void ConstructorEnsuresCommandsArgumentIsSpecified()
+        {
+            var e = Assert.Throws<ArgumentNullException>(() => new ProgramDescriptor("any", "any", null));
+            Assert.Equal("commands", e.ParamName);
+        }
+
+        [Fact]
+        public void ConstructorEnsuresCommandInstancesAreNotNull()
+        {
+            var e = Assert.Throws<ArgumentException>(() => new ProgramDescriptor("any", "any", new CommandDescriptor[] { null }));
+            Assert.Equal("commands", e.ParamName);
+            Assert.Contains("null", e.Message);
+        }
+
+        [Fact]
+        public void ConstructorInitializesCommandsPropertyWithGivenValue()
+        {
+            var commands = new CommandDescriptor[0];
+            var descriptor = new ProgramDescriptor("any", "any", commands);
+            Assert.Same(commands, descriptor.Commands);
+        }
+    }
+}
