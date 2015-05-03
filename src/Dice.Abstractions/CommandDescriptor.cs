391e260ce582e5ae1e405025094f42a7603c2d25
Oleg Sych <oleg_sych@hotmail.com>
2015-05-02 17:04:41 -0700

    - Add CommandDescriptor class.

---
+++ src/Dice.Abstractions/CommandDescriptor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dice.Abstractions
+{
+    public class CommandDescriptor : ItemDescriptor
+    {
+        public CommandDescriptor(string name, string summary, IEnumerable<OptionDescriptor> options) 
+            : base(name, summary)
+        {
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            if (options.Any(item => item == null))
+            {
+                throw new ArgumentException(nameof(options) + " elements must not be null", nameof(options));
+            }
+
+            this.Options = options;
+        }
+
+        public IEnumerable<OptionDescriptor> Options { get; }
+    }
+}
---
+++ test/Dice.Abstractions.Tests/CommandDescriptorTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Dice.Abstractions
+{
+    public class CommandDescriptorTest
+    {
+        [Fact]
+        public void ConstructorInvokesBaseWithAllParameters()
+        {
+            const string expectedName = "Test Name";
+            const string expectedSummary = "Test Summary";
+
+            var descriptor = new CommandDescriptor(expectedName, expectedSummary, Enumerable.Empty<OptionDescriptor>());
+
+            Assert.Equal(expectedName, descriptor.Name);
+            Assert.Equal(expectedSummary, descriptor.Summary);
+        }
+
+        [Fact]
+        public void ConstructorEnsuresOptionsArgumentIsSpecified()
+        {
+            var e = Assert.Throws<ArgumentNullException>(() => new CommandDescriptor("any", "any", null));
+            Assert.Equal("options", e.ParamName);
+        }
+
+        [Fact]
+        public void ConstructorEnsuresOptionsInstancesAreNotNull()
+        {
+            var e = Assert.Throws<ArgumentException>(() => new CommandDescriptor("any", "any", new OptionDescriptor[] { null }));
+            Assert.Equal("options", e.ParamName);
+            Assert.Contains("null", e.Message);
+        }
+
+        [Fact]
+        public void ConstructorInitializesOptionsPropertyWithGivenValue()
+        {
+            var options = new OptionDescriptor[0];
+            var descriptor = new CommandDescriptor("any", "any", options);
+            Assert.Same(options, descriptor.Options);
+        }
+    }
+}
